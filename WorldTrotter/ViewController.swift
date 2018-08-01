86dc918590c2cc0bb658bb6ff8cf8c138ec72240
Jessica Mallian <jlmallian@gmail.com>
2018-08-01 18:20:25 -0500

    programmatically created a view (a blue rectangle - pretty)

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = .blue
+        view.addSubview(firstView)
+    }
 }
 
