c0ecc4b81e78719510b3a7265c2ca8362836acfb
Jessica Mallian <jlmallian@gmail.com>
2018-08-03 14:13:14 -0500

    deleted default ViewController, created ConversionController and associated with the interface in storyboard

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Jessica Mallian on 8/3/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
