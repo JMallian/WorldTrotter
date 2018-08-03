0e59d2b9d9f4b20f207bce30042434ab8934ba62
Jessica Mallian <jlmallian@gmail.com>
2018-08-03 14:38:57 -0500

    added computed property celsiusValue

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    //MARK: properties
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? //optional because user may not have typed a number
     
+    //MARK: computed property
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }else{
+            return nil
+        }
+    }
+    
+    
+    //MARK: functions
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         if let text = textField.text, !text.isEmpty {
             celsiusLabel.text = text
