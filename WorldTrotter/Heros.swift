80c1fbfcf6882397959b6a97ddba5aa9e69eb928
Jessica Mallian <jlmallian@gmail.com>
2018-08-03 20:24:46 -0500

    started making Heros class, decided this is not how I want to go about this, will do tomorrow

---
+++ WorldTrotter/Heros.swift
@@ -0,0 +1,52 @@
+//
+//  Heros.swift
+//  WorldTrotter
+//
+//  Created by Jessica Mallian on 8/3/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import Foundation
+
+class Heros {
+    var theHeros = [
+        "Ana",
+        "Bastion",
+        "Brigitte",
+        "D.Va",
+        "Doomfist",
+        "Genji",
+        "Hanzo",
+        "Junkrat",
+        "Lucio",
+        "McCree",
+        "Mei",
+        "Mercy",
+        "Moira",
+        "Orisa",
+        "Pharah",
+        "Reaper",
+        "Reinhardt",
+        "Roadhog",
+        "Soldier: 76",
+        "Sombra",
+        "Symmetra",
+        "Torbjorn", //TODO: put symbol thing in his name
+        "Tracer",
+        "Widowmaker",
+        "Winston",
+        "Wrecking Ball",
+        "Zarya",
+        "Zenyatta"
+    ]
+    
+    var tanks = [
+        "D.va",
+        "Orisa",
+        "Reinhardt",
+        "Roadhog",
+        "Winston",
+        "Wrecking Ball",
+        "Zarya",
+        ] //TODO: create hero class with properties: name, type (tank/damage/support), weapon type, range/melee, etc
+}
